b9db62cfbb87ba47af407ff31381c2cd062c37e0
Allan Cairns <allan@cairns.id.au>
2025-12-31 12:09:02 +0800

    Add flatpaks and change name

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,6 +17,21 @@ wget https://download3.omnissa.com/software/CART26FQ4_LIN64_RPMPKG_2512/Omnissa-
 dnf5 install -y Omnissa-Horizon-Client-2512-8.17.0-20187591429.x64.rpm
 rm Omnissa-Horizon-Client-2512-8.17.0-20187591429.x64.rpm
 
+# flatpaks
+flatpak install -y --system flathub org.mozilla.Thunderbird
+flatpak install -y --system flathub com.nextcloud.desktopclient.nextcloud
+flatpak install -y --system flathub com.visualstudio.code
+flatpak install -y --system flathub io.github.Faugus.faugus-launcher
+flatpak install -y --system flathub org.libreoffice.LibreOffice
+flatpak install -y --system flathub io.github.flattool.Ignition
+flatpak install -y --system flathub com.github.tchx84.Flatseal
+flatpak install -y --system com.mattjakeman.ExtensionManager 
+flatpak install -y --system org.mozilla.firefox 
+flatpak install -y --system org.gnome.baobab 
+# remove other flatpaks
+flatpak remove -y org.gnome.Calendar
+flatpak remove -y org.gnome.Contacts   
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
